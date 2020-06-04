da2ecf91ae7ed86af59530f79f642198a55df97a
Shivam Sareen <shivamkrsarin1996@gmail.com>
2020-06-04 15:25:44 -0500

    file to run script from windows

--- udacity-p2-create.bat
+++ udacity-p2-create.bat
@@ -1,1 +1,1 @@
-aw cloudformation create-stack --stack-name %1 --template-body file://%2 --parameters file://%3 --region=us-west-2 --capabilities CAPABILITY_IAM
+aws cloudformation create-stack --stack-name %1 --template-body file://%2 --parameters file://%3 --region=us-west-2 --capabilities CAPABILITY_IAM
